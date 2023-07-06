9afe12bda9f19580335fd9ef6b85bc2ea77d2e1a
pingzhu(朱平) <pingzhu@tencent.com>
2023-07-06 23:34:40 +0800

    up

--- lkgpt-service/pkg/flow/englishtextflow.go
+++ lkgpt-service/pkg/flow/englishtextflow.go
@@ -3,6 +3,10 @@ package flow
 type EnglishTextFlow struct {
 }
 
+func (c *EnglishTextFlow) NewTextpromt() string {
+	return "生成一个关于提行李坐飞机场景的英语文章,直接生成内容，不要任何解释描述"
+}
+
 func (c *EnglishTextFlow) Syspromt() string {
 	return `你是一个非常幽默的英语老师，你正在教{name}学习雅思考试中的一个检票登飞机下飞机场景的英语对话或文章,
 	你先打声招呼,然后用英文简单介绍了学习内容，接着输出第一句英文，你依次介绍了这句的重要英文词汇，
